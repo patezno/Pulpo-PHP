8a0d82e6344cfd0583dfc03e8f1afefb7adfa218
Carlos <patezno97@gmail.com>
2019-11-27 23:20:27 +0100

    rest api realizada

---
+++ read.php
@@ -0,0 +1,26 @@
+<?php
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database file
+include_once 'db.php';
+
+$dbname = 'pulpo';
+$collection = 'diario';
+
+
+//DB connection
+$db = new DbManager();
+$conn = $db->getConnection();
+
+// read all records
+$read = new MongoDBDriverQuery($filter, $option);
+
+//fetch records
+$records = $conn->executeQuery("$dbname.$collection", $read);
+
+echo json_encode(iterator_to_array($records));
+
+?>
