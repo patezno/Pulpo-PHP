1d168e0361123772f0036d33229486ff930bb32f
Carlos <patezno97@gmail.com>
2019-11-30 19:30:09 +0100

    inserta datos en bbdd

---
+++ insert.php
@@ -0,0 +1,37 @@
+<?php
+
+function insert() {
+    $dbhost = "localhost";
+    $dbname = "pulpo";
+    $dbusername = "patezno";
+    $dbpassword = "";
+
+    try {
+        $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
+        $jsondata = file_get_contents('diario.json');
+        $diario = json_decode($jsondata, true);
+
+        foreach ($diario as $row) {
+
+            $id = null;
+            $eventos = serialize($row['eventos']);
+            $pulpo = $row['pulpo'];
+
+            $sql = 'INSERT INTO diario(id, eventos, pulpo) VALUES (:id, :eventos, :pulpo);';
+            $stmt = $conn->prepare($sql);
+
+            $stmt->execute([':id' => $id,
+                            ':eventos' => $eventos,
+                            ':pulpo' => $pulpo]);
+        }
+        $conn = null;
+        echo 'Ha funcionado';
+    } catch (PDOException $e) {
+        echo 'No ha funcionado';
+        echo $e->getMessage();
+    }
+}
+
+insert();
+
+?>
