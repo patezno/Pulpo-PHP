50bb078bec11d63cbd721e416133452a66e9ae27
Carlos <patezno97@gmail.com>
2019-11-30 20:51:10 +0100

    eliminado funcion innecesaria

--- read.php
+++ read.php
@@ -1,28 +1,23 @@
 <?php
 
-function read() {
+$dbhost = "localhost";
+$dbname = "pulpo";
+$dbusername = "patezno";
+$dbpassword = "";
 
-    $dbhost = "localhost";
-    $dbname = "pulpo";
-    $dbusername = "patezno";
-    $dbpassword = "";
+try {
 
-    try {
+    $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
+    $statement = $conn->prepare("SELECT * FROM diario");
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $json = json_encode($results);
+    echo $json;
+    $conn = null;
 
-        $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
-        $statement = $conn->prepare("SELECT * FROM diario");
-        $statement->execute();
-        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        $json = json_encode($results);
-        return $json;
-
-    } catch (PDOException $e) {
-        echo 'No ha funcionado';
-        echo $e->getMessage();
-    }
-
+} catch (PDOException $e) {
+    echo 'No ha funcionado';
+    echo $e->getMessage();
 }
-
-read();
 
 ?>
