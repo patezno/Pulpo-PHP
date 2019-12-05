26f60a7acbf848f77d985cb301096786811e92e3
Carlos <patezno97@gmail.com>
2019-12-05 18:39:03 +0100

    back terminado

--- insert.php
+++ insert.php
@@ -9,34 +9,10 @@ try {
         
     $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
     $jsondata = file_get_contents('diario.json');
-    $diario = json_decode($jsondata, true);
+    $sql = 'INSERT INTO diario(diario) VALUES (:diario);';
+    $stmt = $conn->prepare($sql);
 
-    $i = 1;
-    foreach ($diario as $row) {
-
-        $id = $i;
-        $pulpo = $row['pulpo'];
-        $eventos = $row['eventos'];
-
-        $sql = 'INSERT INTO diario(id, pulpo) VALUES (:id, :pulpo);';
-        $stmt = $conn->prepare($sql);
-
-        $stmt->execute([':id' => $id,
-                        ':pulpo' => $pulpo]);
-
-        foreach ($eventos as $evento) {
-
-            $idEvent = null;
-            
-            $sql = 'INSERT INTO evento(id, evento, diario_id) VALUES (:idEvent, :evento, :id);';
-            $stmt = $conn-> prepare($sql);
-
-            $stmt-> execute([':id' => $idEvent,
-                            ':evento' => $evento
-                            ':id' => $id]);
-        }
-        $i++;
-    }
+    $stmt->execute([':diario' => $jsondata]);
     $conn = null;
     echo 'Ha funcionado';
 } catch (PDOException $e) {
