7cfe3fda35a10ac52dccb50e375ea3e7f1971513
Carlos <patezno97@gmail.com>
2019-12-05 18:10:40 +0100

    intento de foreach

--- insert.php
+++ insert.php
@@ -11,18 +11,31 @@ try {
     $jsondata = file_get_contents('diario.json');
     $diario = json_decode($jsondata, true);
 
+    $i = 1;
     foreach ($diario as $row) {
 
-        $id = null;
-        $eventos = serialize($row['eventos']);
+        $id = $i;
         $pulpo = $row['pulpo'];
+        $eventos = $row['eventos'];
 
-        $sql = 'INSERT INTO diario(id, eventos, pulpo) VALUES (:id, :eventos, :pulpo);';
+        $sql = 'INSERT INTO diario(id, pulpo) VALUES (:id, :pulpo);';
         $stmt = $conn->prepare($sql);
 
         $stmt->execute([':id' => $id,
-                        ':eventos' => $eventos,
                         ':pulpo' => $pulpo]);
+
+        foreach ($eventos as $evento) {
+
+            $idEvent = null;
+            
+            $sql = 'INSERT INTO evento(id, evento, diario_id) VALUES (:idEvent, :evento, :id);';
+            $stmt = $conn-> prepare($sql);
+
+            $stmt-> execute([':id' => $idEvent,
+                            ':evento' => $evento
+                            ':id' => $id]);
+        }
+        $i++;
     }
     $conn = null;
     echo 'Ha funcionado';
